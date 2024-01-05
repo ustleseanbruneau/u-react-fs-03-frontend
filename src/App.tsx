93ca312a8f17f5ea2bbb03ddb63e6cfbc87b4f55
LeSean Bruneau <ustbrun9912@gmail.com>
2024-01-05 09:05:01 -0600

    Section 25 - Shelf Page

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ import { Route, Switch, Redirect, useHistory } from 'react-router-dom';
 import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 import { oktaConfig } from './lib/oktaConfig';
 import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
-import { LoginCallback, Security } from '@okta/okta-react';
+import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
 import LoginWidget from './Auth/LoginWidget';
 import { ReviewListPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
+import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -52,6 +53,7 @@ export const App = () => {
             }
           />
           <Route path='/login/callback' component={LoginCallback} />
+          <SecureRoute path='/shelf'> <ShelfPage /> </SecureRoute>
         </Switch>
       </div>
       <Footer/>
