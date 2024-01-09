d18fd8fbfbd6f807d8a2ca7eae7262856f614b10
LeSean Bruneau <ustbrun9912@gmail.com>
2024-01-08 18:51:33 -0600

    Section 27 - Library Services

--- src/layouts/HomePage/components/LibraryServices.tsx
+++ src/layouts/HomePage/components/LibraryServices.tsx
@@ -18,9 +18,13 @@ export const LibraryServices = () => {
           </p>
           <div className='d-grid gap-2 justify-content-md-start mb-4 mb-lg-3'>
           { authState?.isAuthenticated ?
-              <Link type='button' className='btn main-color btn-lg px-4 me-md-2 fw-bold text-white' to='#'>Library Services</Link>
+            <Link to='/messages' type='button' className='btn main-color btn-lg px-4 me-md-2 fw-bold text-white'>
+              Library Services
+            </Link>
             :
-              <Link className='btn main-color btn-lg text-white' to='/login'>Sign up!</Link>
+            <Link className='btn main-color btn-lg text-white' to='/login'>
+              Sign up!
+            </Link>
           }            
           </div>
         </div>
